fa12ce24bed79ecbd552d7283f26f03cc28b946c
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-22 18:53:35 -0600

    Fixed Render database error pt 2

--- backend/db/migrations/20231121235639-create-user.js
+++ backend/db/migrations/20231121235639-create-user.js
@@ -45,9 +45,11 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
