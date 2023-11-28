28529ddc67aef079ca5ed565378584c01e01c10c
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-28 11:03:27 -0600

    Filled demo-group seeder

--- backend/db/seeders/20231128165126-demo-group.js
+++ backend/db/seeders/20231128165126-demo-group.js
@@ -1,25 +1,51 @@
 'use strict';
 
+const { Group } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Group.bulkCreate([
+      {
+        organizerId: 1,
+        name: "Wreckfest",
+        about: "A good old fashioned derby, watch cars slam into other cars and parts go flying!",
+        type: "in-person",
+        private: false,
+        city: "Bonebreaker Valley",
+        state: "New Mexico",
+      },
+      {
+        organizerId: 2,
+        name: "DefaultName",
+        about: "Insert text here.",
+        type: "in-person",
+        private: false,
+        city: "Langley",
+        state: "Virginia",
+      },
+      {
+        organizerId: 3,
+        name: "Fake Event",
+        about: "Free 100000 dollar for visit",
+        type: "online",
+        private: true,
+        city: "Ashburn",
+        state: "Virginia",
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Groups';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      name: { [Op.in]: ['Wreckfest', 'DefaultName', 'Fake Event'] }
+    }, {});
   }
 };
