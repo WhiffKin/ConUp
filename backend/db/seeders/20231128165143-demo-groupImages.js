8cfb644b086164eae0a13e499828330f2a54275f
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-28 11:12:59 -0600

    Filled demo-groupImages seeder

--- backend/db/seeders/20231128165143-demo-groupImages.js
+++ backend/db/seeders/20231128165143-demo-groupImages.js
@@ -1,25 +1,44 @@
 'use strict';
 
+const { GroupImage } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await GroupImage.bulkCreate([
+      {
+        groupId: 1,
+        url: "fakeUrl.com/images/Wreckfest1",
+        preview: true
+      },
+      {
+        groupId: 1,
+        url: "fakeUrl.com/images/Wreckfest2",
+        preview: false
+      },
+      {
+        groupId: 2,
+        url: "fakeUrl.com/images/TestImage",
+        preview: false
+      },
+      {
+        groupId: 3,
+        url: "fakeUrl.com/images/FreeMoney$$$",
+        preview: false
+      },
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'GroupImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      groupId: { [Op.in]: [1, 2, 3] }
+    }, {});
   }
 };
