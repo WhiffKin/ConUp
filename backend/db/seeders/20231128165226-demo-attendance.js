061dc4792d57b28716a3b06e961e9ef3c19b0b95
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-28 13:13:05 -0600

    Filled demo-attendance seeder

--- backend/db/seeders/20231128165226-demo-attendance.js
+++ backend/db/seeders/20231128165226-demo-attendance.js
@@ -1,25 +1,69 @@
 'use strict';
 
+const { Attendance } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Attendance.bulkCreate([
+      {
+        eventId: 1,
+        userId: 1,
+        status: "attending",
+      },
+      {
+        eventId: 1,
+        userId: 2,
+        status: "attending",
+      },
+      {
+        eventId: 1,
+        userId: 3,
+        status: "pending",
+      },
+      {
+        eventId: 2,
+        userId: 1,
+        status: "wait-list",
+      },
+      {
+        eventId: 2,
+        userId: 2,
+        status: "attending",
+      },
+      {
+        eventId: 2,
+        userId: 3,
+        status: "pending",
+      },
+      {
+        eventId: 3,
+        userId: 1,
+        status: "wait-list",
+      },
+      {
+        eventId: 3,
+        userId: 2,
+        status: "pending",
+      },
+      {
+        eventId: 3,
+        userId: 3,
+        status: "wait-list",
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Attendances';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      groupId: { [Op.in]: [1, 2, 3] }
+    }, {});
   }
 };
