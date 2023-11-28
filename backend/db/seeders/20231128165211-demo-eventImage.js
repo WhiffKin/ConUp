224cb3ab05ad6abfc156b7205cddb1bddc6ea4e3
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-28 10:54:01 -0600

    Created seeders, Filled demo-eventImage

---
+++ backend/db/seeders/20231128165211-demo-eventImage.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { EventImages } = require('../models');=
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await EventImages.bulkCreate([
+      {
+        eventId: 1,
+        url: "testUrl.com/Image1",
+        preview: true
+      },
+      {
+        eventId: 2,
+        url: "testUrl.com/Image2",
+        preview: true
+      },
+      {
+        eventId: 3,
+        url: "testUrl.com/Image3",
+        preview: true
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'EventImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      url: { [Op.in]: ['testUrl.com/Image1', 'testUrl.com/Image2', 'testUrl.com/Image3'] }
+    }, {});
+  }
+};
