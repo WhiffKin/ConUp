65ad538cb18ca56d891d53c20ef1ab0f45eba0aa
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-28 11:08:12 -0600

    Filled demo-membership seeder

--- backend/db/seeders/20231128165134-demo-membership.js
+++ backend/db/seeders/20231128165134-demo-membership.js
@@ -1,25 +1,54 @@
 'use strict';
 
+const { Membership } = require('../models');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Membership.bulkCreate([
+      {
+        userId: 2,
+        groupId: 1,
+        status: "co-host"
+      },
+      {
+        userId: 3,
+        groupId: 1,
+        status: "pending"
+      },
+      {
+        userId: 1,
+        groupId: 2,
+        status: "member"
+      },
+      {
+        userId: 3,
+        groupId: 2,
+        status: "pending"
+      },
+      {
+        userId: 1,
+        groupId: 3,
+        status: "pending"
+      },
+      {
+        userId: 2,
+        groupId: 3,
+        status: "pending"
+      },
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Memberships';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      groupId: { [Op.in]: [1, 2, 3] }
+    }, {});
   }
 };
