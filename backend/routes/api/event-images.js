70da396a615a5736b922cc71150afc88096e54e6
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-12-01 16:20:50 -0600

    Implemented route delete /event-image/:imageId

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -2,19 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const { requireAuth } = require('../../utils/auth');
-const { GroupImage, Group } = require('../../db/models');
+const { EventImage, Group, Event } = require('../../db/models');
 
 router.delete("/:imageId",
     requireAuth,
     async (req, res, next) => {
-        const image = await GroupImage.unscoped().findByPk(req.params.imageId);
+        const image = await EventImage.unscoped().findByPk(req.params.imageId);
         if (!image) {
             const err = new Error("Event Image couldn't be found");
             err.status = 403;
             return next(err);
         }
+
         console.log(image)
-        const group = await Group.findByPk(image.groupId);
+        const event = await Event.findByPk(image.eventId);
+        const group = await Group.findByPk(event.groupId);
         let members = await group.getMembers({
             through: {
                 where: {
