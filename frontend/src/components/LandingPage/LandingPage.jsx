9357f7b40392c36d617d098e804d9cf9b55f5bb5
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-12-18 14:36:54 -0600

    Landing Page filled out

---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+import "./LandingPage.css";
+
+function LandingPage() {
+    return (
+        <main>
+            <div className="section1">
+                <div>
+                    <h1>The people platform- Where interests become friendships</h1>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+                </div>
+                <img src="FakeImage"/>
+            </div>
+            <div className="section2">
+                <h3>How Meetup works</h3>
+                <span>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</span>
+            </div>
+            <div className="section3">
+                <NavLink to="/groups">
+                    <img src="FakeImage"/>
+                    <h5>See all groups</h5>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</span>
+                </NavLink>
+                <NavLink to="/events">
+                    <img src="FakeImage"/>
+                    <h5>Find an event</h5>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</span>
+                </NavLink>
+                <NavLink to="/groups/create">
+                    <img src="FakeImage"/>
+                    <h5>Start a group</h5>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.</span>
+                </NavLink>
+            </div>
+            <div className="section4">
+                <button>Join Meetup</button>
+            </div>
+        </main>
+    );
+}
+
+export default LandingPage;
